4b0736358bf9e11fe9b679cc686bd5e7b4b59a0c
Sanaulla Haq <sanaullahaq01@gmail.com>
2025-02-17 16:00:15 +0600

    lc

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
-# from typing import List
 from typing import Optional
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, HTTPException, Header, status
 
-# from app.crud import create_items, get_items
-# from app.models import Item, items
-from app.schemas import UserSchema
+from app.schemas import BookUpdate, User, Book
+from app.books import books
+from typing import List
+
 
 app = FastAPI()
+
 
 @app.get('/')
 async def read_root():
@@ -26,8 +27,6 @@ async def greet(username: Optional[str]="User"):
 
 
 user_list = ["Sanaulla", "Emon", "Akib"]
-
-
 #query params example
 @app.get('/search')
 async def search_for_user(username: str):
@@ -38,10 +37,8 @@ async def search_for_user(username: str):
 
 
 users = []
-
-
 @app.post('/create_user')
-async def create_user(user_data: UserSchema):
+async def create_user(user_data: User):
     new_user = {
         'username': user_data.username,
         'email': user_data.email
@@ -73,3 +70,51 @@ async def get_all_request_headers(
     request_headers["Accept"] = accept
 
     return request_headers
+
+
+#####################################
+####Books Example Stars from here####
+#####################################
+
+@app.get('/books', response_model=List[Book])
+async def get_all_books():
+    return books
+
+
+@app.post('/books', status_code=status.HTTP_201_CREATED)
+async def create_a_book(book_data: Book)->dict:
+    books.append(book_data.model_dump())
+    return books[-1]
+
+
+@app.get('/books/{book_id}')
+async def get_book(book_id: int)->dict:
+    for book in books:
+        if book['id'] == book_id:
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+
+@app.patch('/books/{book_id}')
+async def update_book(book_id: int, book_update_data: BookUpdate)->dict:
+    for book in books:
+        if book['id'] == book_id:
+            # book['title'] = book_update_data.title
+            # book['author'] = book_update_data.author
+            # book['publisher'] = book_update_data.publisher
+            # book['page_count'] = book_update_data.page_count
+            # book['language'] = book_update_data.language
+            for k, v in book_update_data.model_dump().items():
+                book[k] = v
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+
+@app.delete('/books/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int):
+    for book in books:
+        if book['id'] == book_id:
+            books.remove(book)
+            return {}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,24 @@
 from pydantic import BaseModel
 
 
-class UserSchema(BaseModel):
+class User(BaseModel):
     username: str
     email: str
+
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+
+
+class BookUpdate(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
